c2ed4054dc1607b8365295633cf7dc51a8a8bc0d
jksmarthomes <69431936+jksmarthomes@users.noreply.github.com>
2020-11-25 14:32:29 +0530

    Add files via upload

---
+++ precache-manifest.bbac3ad2680da64c9c49e353fc12ebb3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e320fc21ba6d666ea7819b066e57fb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d673c994f146cfcc887",
+    "url": "/static/css/main.b90a6bb3.chunk.css"
+  },
+  {
+    "revision": "3e4c9c354c8f9a5237e5",
+    "url": "/static/js/2.155ffe94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.155ffe94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d673c994f146cfcc887",
+    "url": "/static/js/main.8f8f830e.chunk.js"
+  },
+  {
+    "revision": "b95f120b26924e7dd81e",
+    "url": "/static/js/runtime-main.522e5c97.js"
+  },
+  {
+    "revision": "140006b21539182b9f9d13cf006cd869",
+    "url": "/static/media/apptr.140006b2.png"
+  },
+  {
+    "revision": "620fe809353367a878d1585f27d4b500",
+    "url": "/static/media/board.620fe809.png"
+  },
+  {
+    "revision": "263380d5e590847b2ddabe911c9841bd",
+    "url": "/static/media/final.263380d5.jpg"
+  },
+  {
+    "revision": "ab7f1721352b9b63d2ee4aa1123db3c2",
+    "url": "/static/media/jkhardware.ab7f1721.png"
+  },
+  {
+    "revision": "9e8871213fa29e23a63f92c08280af8f",
+    "url": "/static/media/labelled.9e887121.png"
+  },
+  {
+    "revision": "a7c320d7f1cba771bdd9792c08de08c6",
+    "url": "/static/media/navlogo.a7c320d7.png"
+  },
+  {
+    "revision": "7c39a55fd5b705b99bf362a860141345",
+    "url": "/static/media/productimage.7c39a55f.jpg"
+  },
+  {
+    "revision": "d226649b400dfeb5b74f6791c31d4320",
+    "url": "/static/media/squareboard.d226649b.png"
+  },
+  {
+    "revision": "7fde9f0dac6428336e93b3b1508f6a24",
+    "url": "/static/media/tsf.7fde9f0d.jpg"
+  }
+]);
